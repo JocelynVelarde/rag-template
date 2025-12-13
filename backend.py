c6c92b6d03e0d55c544f24bc3934cf4bd58412f8
NNoorFatima <noor.2205.nf@gmail.com>
2025-12-14 01:54:54 +0500

    done with backend

--- backend.py
+++ backend.py
@@ -24,3 +24,37 @@ def get_vector_store():
         text_key="text"
     )
     return vector_store
+
+
+# covnert text to embeddings 
+def ingest_text(texts):
+    vector_store = get_vector_store()
+    #input to db 
+    docs = Document(page_content=texts)
+    vector_store.add_documents(docs) #the documents are added to the vector store
+    return True
+
+def get_rag_response(query):
+    vector_store = get_vector_store()
+    #define the llm we are using {u can select temp also}
+    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+    retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 3}) #k is the number of documents to retrieve
+
+    #prompt template
+    prompt_template = """Use the following context from the user in order to provide and accurate answer."""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template,
+        input_variables=['context', 'question']
+    )
+
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type='stuff',
+        retriever=retriever,
+        # chain_type_kwargs={"prompt": PROMPT}
+    )
+
+    response = qa_chain.run({"query": query})
+    return response
