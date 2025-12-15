9126f16d7667befa51b01293335863b6fd952dac
Jocelyn <a01285780@itesm.mx>
2025-12-14 18:27:25 -0600

    rag response handling in streamlit

--- home.py
+++ home.py
@@ -19,3 +19,36 @@ with st.sidebar:
         else:
             st.warning("Please enter text")
 
+st.header("Ask anything to the chat from our Knowledge Base")
+
+# Initialize the whole message history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display the chat history
+for message in st.session_state.message:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Handle user input
+prompt = st.chat_input("Ask any question to the chat")
+if prompt:
+    st.chat_message("user").markdown(prompt)
+    st.session_state.message.append({"role": "user", "content": prompt})
+
+    # Generate rag response in here
+    with st.chat_message("ROBOT"):
+        with st.spinner("THINKINGSS"):
+            response_data = rag.get_rag_response(prompt)
+            answer = response_data["answer"]
+            sources = response_data["sources"]
+
+            st.markdown(answer)
+
+            # show sources in a expander
+            with st.expander("Sources"):
+                for i, source in enumerate(sources):
+                    st.markdown(f"**Source {i+1}:** {source.page_content}")
+            # Append the response to the message history
+            st.session_state.message.append({"role": "ROBOT", "content": answer})
+
